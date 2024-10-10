4b5f04c0dba68f0498157e842dd0118688929c1e
Umidjon <79796851+Umidbeck@users.noreply.github.com>
2024-10-10 16:13:04 +0500

    Add files via upload

---
+++ states/reklamaStates.py
@@ -0,0 +1,8 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class ReklamaStates(StatesGroup):
+    reklama = State()
+    reklama1 = State()
+    reklama2 = State()
+    reklama3 = State()
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from . import db_api
+from . import misc
+from . import inference_gfpgan
+from .notify_admins import on_startup_notify
